ae97aff0406365d8d1faf5499345dec9d8873d0a
mohdzaid09 <zaidpeterson1@gmail.com>
2018-01-03 08:27:59 +0800

    first commit

--- student/models.py
+++ student/models.py
@@ -7,7 +7,7 @@ class student(models.Model):
 	PROGRAMMING = 'PR'
 	NETWORKING = 'NT'
 	PCTECH = 'PC'
-	COURSE_CHOICE = (
+	COURSE_CHOICES = (
 		(PROGRAMMING, 'Programming'),
 		(NETWORKING, 'Networking'),
 		(PCTECH, 'PC Technichian'),
@@ -15,7 +15,7 @@ class student(models.Model):
 
 	icnum = models.CharField('IC Number',max_length=12,unique=True,blank=False,null=False)
 	name = models.CharField('Name',max_length=300,blank=False,null=False)
-	course = models.CharField('Coursework',max_length=2,choices=COURSE_CHOICE,default=PCTECH)
+	course = models.CharField('Coursework',max_length=2,choices=COURSE_CHOICES,default=PCTECH)
 	createdby = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 	created_date = models.DateTimeField('Created Date',auto_now_add=True)
 
